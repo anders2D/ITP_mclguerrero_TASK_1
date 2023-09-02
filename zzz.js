39204007f1e970e5ed3f494a4456e39c0fb456b8
mclguerrero <134365120+mclguerrero@users.noreply.github.com>
2023-09-02 15:33:28 -0500

    Add files via upload

---
+++ zzz.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const miBoton = document.getElementById("miBoton");
+    miBoton.addEventListener("click", function () {
+        alert("¡Has hecho clic en el botón!");
+    });
+});
